a7f9b650f7b0feec24e577aa872c290ad82987e2
urviart <urvitrivedi1618@gmail.com>
2018-10-11 22:29:42 -0400

    Created File

---
+++ FLAVR-RECIPE/FLAVR-RECIPE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FLAVR-RECIPE
+//
+//  Created by Urvi Trivedi on 10/11/18.
+//  Copyright © 2018 Urvi Trivedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
